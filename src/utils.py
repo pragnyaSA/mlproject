4e3f7c541889864913f40c563edecf833360d506
pragnyaSA <131436691+pragnyaSA@users.noreply.github.com>
2026-04-28 00:50:05 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.metrics import r2_score
-#from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -25,19 +25,29 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)        
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for model_name, model in models.items():
-            
+
+            para = param.get(model_name, {})
+
+            if para:
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+
+                model.set_params(**gs.best_params_)
+
             # Train model
             model.fit(X_train, y_train)
 
-            # Predict
+            # Predictions
+            y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
-            # R2 Score
+            # Scores
+            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[model_name] = test_model_score
@@ -45,7 +55,8 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)           
+        raise CustomException(e, sys)
+            
 
- 
+    
     
