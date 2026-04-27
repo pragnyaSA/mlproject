bdb91d7d9694903ee2ba02ebf772f50b22957cfb
pragnyaSA <131436691+pragnyaSA@users.noreply.github.com>
2026-04-28 00:06:11 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,10 @@ import dill
 
 import numpy as np
 import pandas as pd
+import pickle
+from sklearn.metrics import r2_score
+#from sklearn.model_selection import GridSearchCV
+
 
 
 from src.exception import CustomException
@@ -21,3 +25,27 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)        
 
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            
+            # Train model
+            model.fit(X_train, y_train)
+
+            # Predict
+            y_test_pred = model.predict(X_test)
+
+            # R2 Score
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)           
+
+ 
+    
